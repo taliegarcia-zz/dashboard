aa983f4be0f132c736cfe277a76d15c86f69f569
taliegarcia <taliegarcia@gmail.com>
2016-06-24 13:36:36 -0700

    table; got tableController to load mockData into a table. using main html page only.

--- app.js
+++ app.js
@@ -23,8 +23,25 @@ dashboardApp.controller('mainController', ['$scope', '$log', function($scope, $l
 }]);
 
 // TABLE CONTROLLER
-dashboardApp.controller('tableController', ['$scope', '$log', function($scope, $log){
+var mockData = [{
+                  name: "Raphael Bryant",
+                  email: "libero@egetdictumplacerat.ca",
+                  company: "Ridiculus Mus Corp.",
+                  id: "93EC3B4E-2B6B-88E2-2AD3-441E4E16AF6E",
+                  score: 16
+                },
+                {
+                  name: "Jennifer Levine",
+                  email: "diam@tinciduntduiaugue.org",
+                  company: "Aliquet LLP",
+                  id: "4BF750A0-880B-34FB-7117-36A08B731257",
+                  score: 15
+                }]
 
-  $scope.controllerName = 'tableController';
+
+dashboardApp.controller('tableController', ['$scope', function($scope){
+    $scope.controllerName = 'tableController';
+
+    $scope.users = mockData;
 
 }]);
